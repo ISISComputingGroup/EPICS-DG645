4153b760bb01a3b9b7914b3bea4333fee3e4a4f8
Jack Doughty <jackb.doughty@btinternet.com>
2025-01-16 14:05:50 +0000

    Disables use of auto mode for now

--- system_tests/tests/dg645_llt.py
+++ system_tests/tests/dg645_llt.py
@@ -35,57 +35,47 @@ class Dg645LLTTests(unittest.TestCase):
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
 
     def test_Summed_Value(self):
-        self.ca.set_pv_value("OFFSET", 10)
-        self.ca.set_pv_value("DELAY", 20)
-        self.ca.assert_that_pv_is("SUMMED_VALUE", 30)
+        self.ca.set_pv_value("DELAY", 10)
+        self.ca.set_pv_value("OFFSET", 20)
+        self.ca.set_pv_value("SET.PROC", "1")
+        self.ca.assert_that_pv_is("ADelayAO", 0.00003) 
 
     def test_Summed_Delay(self):
-        self.ca.set_pv_value("ADelayAO", 10)
+        self.ca.set_pv_value("ADelayAO", 0.00001)
         self.ca.set_pv_value("CDelayAO", 0.00002)
-        
         self.ca.assert_that_pv_is("SUMMED_DELAY", 30)
-
-    def test_Summed_Final(self):
-        self.ca.set_pv_value("CDelayAO", 0.00001)
-        
-        self.ca.set_pv_value("OFFSET", 10)
-        self.ca.set_pv_value("DELAY", 30)
-        self.ca.set_pv_value("MODE", "auto")
-        self.ca.set_pv_value("SET.PROC", "1")
-        self.ca.assert_that_pv_is("ADelayAI", 50) # Test that A delay = cdelay + delay + offset
-        self.ca.assert_that_pv_is("SUMMED_DELAY", 60) # 
 
     @parameterized.expand(
         [(0, 39900, 100, 1)]
     )
     def test_When_In_Error_Can_Not_Set_Delay(self, cdelay, delay, offset, err):
         self.ca.set_pv_value("CDelayAO", cdelay)
-        
+        self.ca.set_pv_value("ADelayAO", 0)
         self.ca.set_pv_value("OFFSET", offset)
         self.ca.set_pv_value("DELAY", delay)
         self.ca.set_pv_value("MODE", "1")
         self.ca.set_pv_value("SET.PROC", "1")
         self.ca.assert_that_pv_is("ERROR", err)
-        self.ca.set_pv_value("ADelayAO", 0)
+        self.ca.assert_that_pv_is("ADelayAO", 0)
 
-    def test_Mode_Zero_Change_Mode_To_One(self):
-        self.ca.set_pv_value("DELAY", 10)
-        self.ca.set_pv_value("OFFSET", 5)
-        self.ca.set_pv_value("MODE", "auto")
-        self.ca.assert_that_pv_is("SET_MODE", 1)
+    # Bring back for auto use
+    # def test_Mode_Auto_Change_Mode_To_One(self):
+    #     self.ca.set_pv_value("DELAY", 10)
+    #     self.ca.set_pv_value("OFFSET", 5)
+    #     self.ca.set_pv_value("MODE", "auto")
+    #     self.ca.assert_that_pv_is("SET_MODE", 1)
 
-    def test_Mode_Zero_Change_Mode_To_Two(self):
-        self.ca.set_pv_value("DELAY", 5)
-        self.ca.set_pv_value("OFFSET", 5)
-        self.ca.set_pv_value("MODE", "auto")
-        self.ca.assert_that_pv_is("SET_MODE", 2)
+    # def test_Mode_Auto_Change_Mode_To_Two(self):
+    #     self.ca.set_pv_value("DELAY", 5)
+    #     self.ca.set_pv_value("OFFSET", 5)
+    #     self.ca.set_pv_value("MODE", "auto")
+    #     self.ca.assert_that_pv_is("SET_MODE", 2)
 
     @parameterized.expand(
         [(0, 39900, 100, 1), (0.04, 0, 0, 1), (0.00001, -1, 0, 1), (0.00001, -5, 5, 0)]
     )
     def test_Mode_One_Error_Check(self, cdelay, delay, offset, err):
         self.ca.set_pv_value("CDelayAO", cdelay)
-        
         self.ca.set_pv_value("OFFSET", offset)
         self.ca.set_pv_value("DELAY", delay)
         self.ca.set_pv_value("MODE", "1")
@@ -103,7 +93,6 @@ class Dg645LLTTests(unittest.TestCase):
     )
     def test_Mode_Two_Error_Check(self, cdelay, delay, offset, err):
         self.ca.set_pv_value("CDelayAO", cdelay)
-        
         self.ca.set_pv_value("OFFSET", offset)
         self.ca.set_pv_value("DELAY", delay)
         self.ca.set_pv_value("MODE", "2")
